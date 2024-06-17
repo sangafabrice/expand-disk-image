07cf2cd61f93fb75e03b12a916031753c3367aa3
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-06-17 16:01:29 +0100

    feat: Add HideDrive parameter to merge settings

--- Expand-IsoDiskImage.ps1
+++ Expand-IsoDiskImage.ps1
@@ -8,6 +8,8 @@ Specifies the path to an existing ISO file.
 .PARAMETER DestinationPath
 Specifies the destination directory where the files are extracted.
 The default destination folder path is the path to the ISO file without the extension.
+.PARAMETER HideDrive
+Specifies that the virtual drive be hidden from the file explorer application.
 .PARAMETER Open
 Specifies that the script opens the destination folder when the extraction completes.
 #>
@@ -19,10 +21,12 @@ Param (
   [string] $ImagePath,
   [ValidateNotNullOrEmpty()]
   [string] $DestinationPath = $ImagePath -ireplace '\.iso$',
+  [switch] $HideDrive,
   [switch] $Open
 )
 # Convert the ISO file path to absolute path.
 $ImagePath = (Get-Item $ImagePath).FullName
+If ($HideDrive) {
 # The Temp folder must be NTS formated for mounting disk images.
 # This is a design decision since we can assign a drive letter to the internal volumes
 # of the disk image and use the roots of those volumes as temporary mount directories.
@@ -34,6 +38,7 @@ If ((Get-Volume -DriveLetter (Get-Item $Env:TEMP).PSDrive.Name).FileSystem -ine 
 While (Test-Path ($TempMountDirPath = "$Env:Temp\MountDir-$(New-Guid)")) { }
 # The TEMP subdirectory is created here to check write right access.
 [void] (New-Item -Path $TempMountDirPath -ItemType Directory -ErrorAction Stop)
+}
 # Modify the destination path if it carries info of a file.
 $SkipIndex = 0
 While (Test-Path $DestinationPath -PathType Leaf) {
@@ -42,7 +47,8 @@ While (Test-Path $DestinationPath -PathType Leaf) {
 # Create the destination folder and convert its path to absolute.
 $DestinationPath = (New-Item $DestinationPath -ItemType Directory -Force -ErrorAction Stop).FullName
 # Mount the ISO disk image and store objects to variables.
-$DiskImageVolume = ($DiskImage = Mount-DiskImage $ImagePath -NoDriveLetter) | Get-Volume
+$DiskImageVolume = ($DiskImage = Mount-DiskImage $ImagePath -NoDriveLetter:$HideDrive) | Get-Volume
+If ($HideDrive) {
 Try {
   # Append to the temporary mount directory path the disk label segment.
   $TempMountDirPath = (New-Item -Path "$TempMountDirPath\$($DiskImageVolume.FileSystemLabel)" -ItemType Directory -ErrorAction Stop).FullName
@@ -50,6 +56,9 @@ Try {
 Catch { }
 # Mount the disk image volume in the temporary mount directory.
 mountvol.exe $TempMountDirPath $($DiskImageVolume.Path)
+} Else {
+  $TempMountDirPath = $DiskImageVolume.DriveLetter + ':\'
+}
 # Copy the temporary mount directory to the destination folder.
 # The CopyHere() method of the Windows Shell COM API is responsible for checking the 
 # available space in the destination folder, handling file overwrites, and displaying a progress bar.
@@ -58,7 +67,9 @@ NameSpace($DestinationPath).
 CopyHere($TempMountDirPath)
 # Clean up disk and volume settings.
 [void] ($DiskImage | Dismount-DiskImage)
+If ($HideDrive) {
 mountvol.exe /r
+}
 # Open the destination folder if required.
 If ($Open) {
   explorer.exe $DestinationPath
